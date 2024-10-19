cb1521b9ee6feb70d1fa30747025e8cdfe477b2f
xsmzdd <111579783+xsmzdd@users.noreply.github.com>
2024-10-19 13:34:32 +0800

    Update remove_extra_spaces.sh

--- remove_extra_spaces.sh
+++ remove_extra_spaces.sh
@@ -7,3 +7,9 @@ TARGET_FILE="/etc/systemd/system/nezha-agent.service"
 sed -i 's/\([[:space:]]\{2,\}\)\(--tls\|"\--tls"\)/ \2/g' "$TARGET_FILE"
 
 echo "已处理多余空格，保留一个空格在 '--tls' 或 '\"--tls\"' 前面。"
+
+# 重新加载 systemd 并重启服务
+sudo /usr/bin/systemctl daemon-reload && \
+sudo /usr/bin/systemctl restart nezha-agent.service
+
+echo "服务已重启。"
