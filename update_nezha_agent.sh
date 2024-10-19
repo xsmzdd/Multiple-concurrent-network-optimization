4b09cebaa6b16ae55da39d43b58b7828bf7a61a1
xsmzdd <111579783+xsmzdd@users.noreply.github.com>
2024-10-19 13:15:54 +0800

    Update update_nezha_agent.sh

--- update_nezha_agent.sh
+++ update_nezha_agent.sh
@@ -4,16 +4,16 @@
 SERVICE_FILE="/etc/systemd/system/nezha-agent.service"
 
 # 检查文件中是否包含特定的 ExecStart 行
-if grep -q 'ExecStart=/opt/nezha/agent/nezha-agent -s nzip.moneytaoist.sbs:5555 -p [^ ]*  --disable-auto-update' "$SERVICE_FILE"; then
-    echo "匹配到第一种 ExecStart 格式，执行相关命令..."
+if grep -q 'ExecStart=.*nzip.moneytaoist.sbs:5555' "$SERVICE_FILE"; then
+    echo "匹配到没有引号的 ExecStart 格式，执行相关命令..."
     source ~/.bashrc && \
     sudo sed -i 's|nzip.moneytaoist.sbs:5555|www.money-taoist.icu:443|g' "$SERVICE_FILE" && \
     sudo sed -i 's|--disable-auto-update|--tls --disable-auto-update|g' "$SERVICE_FILE" && \
     sudo /usr/bin/systemctl daemon-reload && \
     sudo /usr/bin/systemctl restart nezha-agent.service
 
-elif grep -q 'ExecStart=/opt/nezha/agent/nezha-agent "-s" "nzip.moneytaoist.sbs:5555" "-p" "[^"]*"  --disable-auto-update' "$SERVICE_FILE"; then
-    echo "匹配到第二种 ExecStart 格式，执行相关命令..."
+elif grep -q 'ExecStart=.*"nzip.moneytaoist.sbs:5555"' "$SERVICE_FILE"; then
+    echo "匹配到有引号的 ExecStart 格式，执行相关命令..."
     source ~/.bashrc && \
     sudo sed -i 's|nzip.moneytaoist.sbs:5555|www.money-taoist.icu:443|g' "$SERVICE_FILE" && \
     sudo sed -i 's|--disable-auto-update|"--tls" --disable-auto-update|g' "$SERVICE_FILE" && \
