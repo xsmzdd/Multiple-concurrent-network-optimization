b38fd6225618a0a16e161bac7ba4a3cb27e042cb
xsmzdd <111579783+xsmzdd@users.noreply.github.com>
2024-10-19 13:12:16 +0800

    Create update_nezha_agent.sh

---
+++ update_nezha_agent.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 定义目标文件路径
+SERVICE_FILE="/etc/systemd/system/nezha-agent.service"
+
+# 检查文件中是否包含特定的 ExecStart 行
+if grep -q 'ExecStart=/opt/nezha/agent/nezha-agent -s nzip.moneytaoist.sbs:5555 -p [^ ]*  --disable-auto-update' "$SERVICE_FILE"; then
+    echo "匹配到第一种 ExecStart 格式，执行相关命令..."
+    source ~/.bashrc && \
+    sudo sed -i 's|nzip.moneytaoist.sbs:5555|www.money-taoist.icu:443|g' "$SERVICE_FILE" && \
+    sudo sed -i 's|--disable-auto-update|--tls --disable-auto-update|g' "$SERVICE_FILE" && \
+    sudo /usr/bin/systemctl daemon-reload && \
+    sudo /usr/bin/systemctl restart nezha-agent.service
+
+elif grep -q 'ExecStart=/opt/nezha/agent/nezha-agent "-s" "nzip.moneytaoist.sbs:5555" "-p" "[^"]*"  --disable-auto-update' "$SERVICE_FILE"; then
+    echo "匹配到第二种 ExecStart 格式，执行相关命令..."
+    source ~/.bashrc && \
+    sudo sed -i 's|nzip.moneytaoist.sbs:5555|www.money-taoist.icu:443|g' "$SERVICE_FILE" && \
+    sudo sed -i 's|--disable-auto-update|"--tls" --disable-auto-update|g' "$SERVICE_FILE" && \
+    sudo /usr/bin/systemctl daemon-reload && \
+    sudo /usr/bin/systemctl restart nezha-agent.service
+else
+    echo "没有匹配到预定义的 ExecStart 格式，未执行任何操作。"
+fi
